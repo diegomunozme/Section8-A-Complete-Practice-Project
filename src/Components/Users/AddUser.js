c4cc85c04b10c73cd74d07cd553cd5fad1602e1a
diegomunozme <diego.munoz.me@gmail.com>
2022-05-24 05:28:38 -0700

    implemented addUser states, button and card

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,20 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./AddUser.module.css";
 import Card from "../UI/Card";
+import Button from "../UI/Button";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    // We want to add checks in our code for age and name (not empty and x>1)
+    if(enteredUsername.trim().length === 0 | enteredAge.trim().length === 0){
+        return;
+    }
+    //Adding a plus before enteredAge ensures that it will always be a int data type
+    if(+enteredAge < 1){
+        return;
+    }
+    console.log(enteredUsername, enteredAge);
+    setEnteredAge('');
+    setEnteredUsername('');
   };
 
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
   return (
     <Card className={classes.input}>
+    {/* Add user handler ensures we're running on client-side*/}
       <form onSubmit={addUserHandler}>
         <label htmlfor="username">Username</label>
-        <input id="username" type="text" />
+        <input 
+            id="username"
+            type="text"
+            value = {enteredUsername}
+            onChange= {usernameChangeHandler} />
         <label htmlfor="age">Age</label>
-        <input id="age" type="Number" />
-        <button type="submit">Add User</button>
+        <input 
+            id="age" 
+            type="Number" 
+            value = {enteredAge}
+            onChange={ageChangeHandler}
+             />
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
