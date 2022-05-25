3c38ff67f4128e18cf591029fa1eb0bc5f51aa54
diegomunozme <diego.munoz.me@gmail.com>
2022-05-25 04:58:05 -0700

    lifted the user and age to app.js via props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import AddUser from './Components/Users/AddUser';
+import React, { useState } from "react";
+import AddUser from "./Components/Users/AddUser";
+import UsersList from "./Components/Users/UsersList";
 
 function App() {
+  const [usersList,setUsersList] = useState([])
+
+  const addUserHandler = (userName, userAge) => {
+    setUsersList((prevUsersList) => {
+      return[...prevUsersList, {name:userName, age:userAge}];
+    });
+  }
   return (
     <div>
-      <AddUser/>
+      {/*
+      NOTES: 
+        AddUsers contains state and input functionality for client side rendering
+        UsersList displays the list of user objects whos attributes been inputted in AddUser*/}
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
---
+++ src/Components/Users/UsersList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classes from "./UsersList.module.css";
+
+import Card from "../UI/Card";
+
+const UsersList = (props) => {
+  return (
+    <Card className={classes.users}>
+      <ul>
+        {/* users is an array of user objects, which 
+      every object has a name and age property */}
+        {props.users.map((user) => (
+          <li>
+            {user.name} ({user.name} years old){" "}
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+};
+
+
+export default UsersList;
